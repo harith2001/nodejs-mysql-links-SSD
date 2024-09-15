790b677ea38f924c407e7c9ab9335f965a277cf5
harith2001 <danula243@gmail.com>
2024-09-15 21:44:48 +0530

    Fixed : Cookie without SameSite Attribute

--- src/middlewares/protectedRoutes.js
+++ src/middlewares/protectedRoutes.js
@@ -4,6 +4,8 @@ export const isLoggedIn = (req, res, next) => {
 };
 
 export const isNotLoggedIn = (req, res, next) => {
-  if (req.isAuthenticated()) return res.redirect("/links");
+  if (req.isAuthenticated()){
+  return res.redirect("/links");
+  }
   return next();
-}
+};
