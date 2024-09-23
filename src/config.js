221af3f9d7fadd8cac4d8023900a7c07c590abd0
harith2001 <danula243@gmail.com>
2024-09-24 01:58:18 +0530

    config updated

--- src/config.js
+++ src/config.js
@@ -3,16 +3,15 @@ config();
 
 export const database = {
   connectionLimit: 10,
-  host: process.env.DATABASE_HOST || "localhost",
-  user: process.env.DATABASE_USER || "fazt",
-  password: process.env.DATABASE_PASSWORD || "mypassword",
-  database: process.env.DATABASE_NAME || "linksdb",
-  port: process.env.DATABASE_PORT || 3306,
+  host: process.env.DATABASE_HOST ,
+  user: process.env.DATABASE_USER ,
+  password: process.env.DATABASE_PASSWORD ,
+  database: process.env.DATABASE_NAME,
+  port: process.env.DATABASE_PORT ,
 };
 
-export const port = process.env.PORT || 4000;
-
-export const SECRET = process.env.SECRET || 'some secret key';
+export const port = process.env.PORT;
+export const SECRET = process.env.SECRET ;
 
 export const GOOGLE_CLIENT_ID= process.env.GOOGLE_CLIENT_ID 
 export const GOOGLE_CLIENT_SECRET=process.env.GOOGLE_CLIENT_SECRET
