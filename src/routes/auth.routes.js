94b12e6358d475abc235858600fb82c040199a13
harith2001 <danula243@gmail.com>
2024-09-25 18:32:32 +0530

    unused comments cleared

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,8 +4,7 @@ import {
   signUp,
   renderSignIn,
   signIn,
-  logout,
-  googleCallback,
+  logout
 } from "../controllers/auth.controller.js";
 import { validator } from "../middlewares/validator.middleware.js";
 import { signinSchema, signupSchema } from "../schemas/auth.schema.js";
@@ -23,19 +22,13 @@ router.post("/signin", validator(signinSchema), signIn);
 
 // Google OAuth - Login
 router.get("/auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
-
-// Google OAuth - Callback
-// router.get("/auth/google/callback",passport.authenticate("google",{ failureRedirect: "/signin" }),googleCallback);
+;
 router.get('/auth/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
+  passport.authenticate('google', { failureRedirect: '/sigin' }),
   (req, res) => {
     res.redirect('/links');
   }
 );
-// router.get("/auth/google/callback",passport.authenticate("google"),
-//   (req, res) => {
-//     res.redirect("/links");
-//   });
 
 // Logout
 router.get("/logout", logout);
